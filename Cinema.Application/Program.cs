64778a6df56169ad541e60e5be2e8dddfe60637c
Daniel Bringel Gusmão <danielgusmao@gmail.com>
2022-09-19 09:44:22 -0300

    Bearer no Swagger

--- Cinema.Application/Program.cs
+++ Cinema.Application/Program.cs
@@ -16,6 +16,8 @@ using System.Net.WebSockets;
 using System.Net;
 using System.Text;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -74,9 +76,37 @@ builder.Services.AddAuthentication(x =>
         };
     });
 
+
+builder.Services.AddSwaggerGen(setup =>
+{
+    // Include 'SecurityScheme' to use JWT Authentication
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() }
+    });
+
+});
+
+
 var app = builder.Build();
-
-
 
 
 // Configure the HTTP request pipeline.
